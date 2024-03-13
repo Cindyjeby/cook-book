6c4849626cb7d8580e9c03731b3bc4491c800c87
cindyjeby <cindyjeby@gmail.com>
2024-03-13 16:46:55 +0300

    connecting frontend registration with backend api using express

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from './layout';
 import IndexPage from './pages/indexPage';
 import LoginPage from './pages/loginPage';
+import RegisterPage from './pages/registerPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -1,5 +1,11 @@
 export default function LoginPage() {
     return(
-        <div>Login Page</div>
+        <form className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="username" required />
+            <input type="email" placeholder="email" required />
+            <input type="password" placeholder="password" required />
+            <button>Login</button>
+        </form>
     );
 }
