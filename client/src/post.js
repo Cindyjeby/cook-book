162a8788972b8e0bb3f5ba853997d9bb1534b7d7
cindyjeby <cindyjeby@gmail.com>
2024-04-01 20:15:11 +0300

    display cover picture, username, timestaps

--- client/src/post.js
+++ client/src/post.js
@@ -1,16 +1,16 @@
-import {formatISO9075} from "date-fns";
+import {format} from "date-fns";
 
-export default function Post({title,summary,cover,content,createdAt}) {
+export default function Post({title,summary,cover,content,createdAt, author}) {
     return (
         <div className="post">
         <div className="image">
-          <img src="https://i.pinimg.com/564x/66/69/aa/6669aa09bc7baabaf050f80c86416806.jpg"></img>
+          <img src={'http://localhost:4000/'+cover}></img>
         </div>
         <div className="texts">
           <h2>{title}</h2>
           <p className="info">
-            <a className="author">Queen of Spades</a>
-            <time>{formatISO9075(new Date(createAt))}</time>
+            <a className="author">{author.username}</a>
+            <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
           </p>
           <p className="summary">{summary}</p>
         </div>
