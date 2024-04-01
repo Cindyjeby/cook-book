dc35361c1e46eadc867333da8bb683f293dcae3d
cindyjeby <cindyjeby@gmail.com>
2024-04-01 18:37:34 +0300

    displaying created posts

--- client/src/post.js
+++ client/src/post.js
@@ -1,16 +1,18 @@
-export default function Post() {
+import {formatISO9075} from "date-fns";
+
+export default function Post({title,summary,cover,content,createdAt}) {
     return (
         <div className="post">
         <div className="image">
           <img src="https://i.pinimg.com/564x/66/69/aa/6669aa09bc7baabaf050f80c86416806.jpg"></img>
         </div>
         <div className="texts">
-          <h2>Easy Saucy Ramen Noodles</h2>
+          <h2>{title}</h2>
           <p className="info">
             <a className="author">Queen of Spades</a>
-            <time>2024-03-08 14:49</time>
+            <time>{formatISO9075(new Date(createAt))}</time>
           </p>
-          <p className="summary">Here's an easy saucy ramen noodles recipe with a thick and flavourful sauce! You can also add in whatever veggies of your choice or even some tofu and mushrooms!The sauce is really versatile so you can opt to even use it for stir-fries. Slurp away and enjoy!</p>
+          <p className="summary">{summary}</p>
         </div>
       </div>
     );
